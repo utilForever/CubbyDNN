331f8791c0889355c00675cb4d192cf8fc2fec80
jwkim98 <jwkim98@kaist.ac.kr>
2018-11-20 19:51:45 +0900

    Simple example added

--- Sources/cubbydnn/Test.cpp
+++ Sources/cubbydnn/Test.cpp
@@ -5,15 +5,20 @@
 using namespace cubby_dnn;
 int Add(int a, int b)
 {
-    File_stream<int> file_stream;
-    auto input_tensor1 = Generate<int>::placeHolder(std::vector<int>{ 1, 1, 1 }, file_stream,
-                                                    "test placeHolder operation");
-    auto input_tensor2 = Generate<int>::weight(std::vector<int>{ 1, 1, 1 }, true, "test weight operation");
-
-    auto multiplied_tensor1 = Operate<int>::matMul(input_tensor1, input_tensor2, "test matMul operation");
-
-    Final<int>::wrapper(multiplied_tensor1, "test wrapper operation");
+    // Example of multiplying two tensors of shape {1,1,1}
     return a + b;
-
 }
 
+void simple_Example()
+{
+    File_stream<int> file_stream;
+    auto input_tensor1 = Generate<int>::placeHolder(
+        std::vector<int>{ 1, 1, 1 }, file_stream, "test placeHolder operation");
+    auto input_tensor2 = Generate<int>::weight(std::vector<int>{ 1, 1, 1 },
+                                               true, "test weight operation");
+
+    auto multiplied_tensor1 = Operate<int>::matMul(input_tensor1, input_tensor2,
+                                                   "test matMul operation");
+
+    Final<int>::wrapper(multiplied_tensor1, "test wrapper operation");
+}
--- Tests/UnitTests/SimpleTest.cpp
+++ Tests/UnitTests/SimpleTest.cpp
@@ -6,3 +6,7 @@ TEST(SimpleTest, Add)
     EXPECT_EQ(5, Add(2, 3));
     std::cout << "called Test" << std::endl;
 }
+
+TEST(SimpleExample, simple_Example)
+{
+}
