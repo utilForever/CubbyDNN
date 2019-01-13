0604bc0494327baa5d86a1ca90c1913154fbed44
Chris Ohk <utilForever@gmail.com>
2019-01-14 01:58:27 +0900

    [WIP] refactor: Create TensorObject class

---
+++ Includes/cubbydnn/Tensor/TensorObject.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYDNN_TENSOR_OBJECT_HPP
+#define CUBBYDNN_TENSOR_OBJECT_HPP
+
+#include <cubbydnn/Tensor/TensorData.hpp>
+#include <cubbydnn/Tensor/TensorInfo.hpp>
+#include <cubbydnn/Tensor/TensorShape.hpp>
+
+#include <memory>
+#include <mutex>
+
+namespace CubbyDNN
+{
+//!
+//! \brief TensorObject class.
+//!
+//! This class will represent graph at runtime, and stores actual data used in
+//! graph execution.
+//!
+class TensorObject
+{
+ public:
+    TensorObject(std::size_t size, const TensorShape shape, long from, long to);
+    ~TensorObject();
+    TensorObject(const TensorObject& obj);
+    TensorObject(TensorObject&& obj) noexcept;
+    TensorObject& operator=(const TensorObject& obj);
+    TensorObject& operator=(TensorObject&& obj) noexcept;
+
+    const TensorInfo& Info() const;
+    const std::vector<float> Data() const;
+    const std::unique_ptr<TensorData> DataPtr();
+    TensorShape DataShape() const;
+
+    void MakeImmutable();
+
+    private : TensorInfo m_info;
+    std::unique_ptr<TensorData> m_data;
+    std::mutex m_dataMtx;
+};
+}  // namespace CubbyDNN
+
+#endif  // CUBBYDNN_TENSOR_OBJECT_HPP
