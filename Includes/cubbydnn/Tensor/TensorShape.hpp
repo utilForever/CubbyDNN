94935db6ca9122b66f557ef6df6f2d115453aec6
Chris Ohk <utilForever@gmail.com>
2019-01-13 16:13:34 +0900

    refactor: Create TensorShape class

---
+++ Includes/cubbydnn/Tensor/TensorShape.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYDNN_TENSOR_SHAPE_HPP
+#define CUBBYDNN_TENSOR_SHAPE_HPP
+
+#include <vector>
+
+namespace CubbyDNN
+{
+class TensorShape
+{
+ public:
+    TensorShape() = default;
+    TensorShape(long rows, long columns, long depth);
+
+    bool operator==(const TensorShape& shape) const;
+    bool operator!=(const TensorShape& shape) const;
+
+    std::size_t Size() const;
+    bool IsEmpty() const;
+    long Row() const;
+    long Col() const;
+    long Depth() const;
+
+ private:
+    std::vector<long> m_shapeVector;
+    std::size_t m_totalSize = 0;
+};
+}  // namespace CubbyDNN
+
+#endif  // CUBBYDNN_TENSOR_SHAPE_HPP
