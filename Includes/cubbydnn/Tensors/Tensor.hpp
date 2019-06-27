08a3bb9e35734987f2dbf5ce5a253496e7a75bb6
jwkim98 <jwkim98@kaist.ac.kr>
2019-06-27 22:00:47 +0900

    add : AllocateTensor function for allocating empty tensor

--- Includes/cubbydnn/Tensors/Tensor.hpp
+++ Includes/cubbydnn/Tensors/Tensor.hpp
@@ -7,7 +7,7 @@
 #ifndef CUBBYDNN_TENSOR_DATA_HPP
 #define CUBBYDNN_TENSOR_DATA_HPP
 
-#include <cubbydnn/Tensors/TensorDataInfo.hpp>
+#include <cubbydnn/Tensors/TensorInfo.hpp>
 
 #include <atomic>
 #include <memory>
@@ -15,27 +15,31 @@
 
 namespace CubbyDNN
 {
-
 /**
  * TensorData class contains data vector for processing
  * with attributes describing it
  * @tparam T : type of data this tensorData contains
  */
-struct Tensor'Data
+struct Tensor
 {
-    TensorData(void* Data, NumberSystem numberSystem, const TensorDataInfo& info);
+    Tensor(std::unique_ptr<void> Data, TensorInfo info);
     /// Data vector which possesses actual data
-    void* DataPtr;
-    /// Type of this ptr
-    NumberSystem numberSystem;
+    std::unique_ptr<void> DataPtr;
     /// Shape of this tensorData
-    TensorDataInfo Info;
-    // TensorType Type;
+    TensorInfo Info;
 };
 
 
+using TensorPtr = typename std::unique_ptr<Tensor>;
 
-using TensorDataPtr = typename std::unique_ptr<TensorData>;
+/**
+ * Builds empty Tensor so data can be put potentially
+ * @param info
+ * @return
+ */
+static TensorPtr AllocateTensor(TensorInfo info);
+
+//TODO : Implement methods for Initializing the Tensor
 
 }  // namespace CubbyDNN
 
--- Sources/cubbydnn/Tensors/Tensor.cpp
+++ Sources/cubbydnn/Tensors/Tensor.cpp
@@ -1,17 +1,23 @@
 /** Copyright (c) 2019 Chris Ohk, Justin Kim
  *
-* We are making my contributions/submissions to this project solely in our
-* personal capacity and are not conveying any rights to any intellectual
-* property of any third parties.
-*/
+ * We are making my contributions/submissions to this project solely in our
+ * personal capacity and are not conveying any rights to any intellectual
+ * property of any third parties.
+ */
 
-#include <cubbydnn/Tensors/TensorData.hpp>
+#include <cubbydnn/Tensors/Tensor.hpp>
 
 namespace CubbyDNN
 {
-    TensorData::TensorData(void* Data, NumberSystem numberSystem, const TensorDataInfo& info)
-    :DataPtr(Data), numberSystem(numberSystem), Info(info)
-    {
-
-    }
+Tensor::Tensor(std::unique_ptr<void> Data, TensorInfo info)
+    : DataPtr(std::move(Data)), Info(std ::move(info))
+{
 }
+
+static TensorPtr AllocateTensor(TensorInfo info){
+    auto byteSize = info.ByteSize();
+    auto dataPtr = std::make_unique<void>(byteSize);
+    return std::make_unique<Tensor>(std::move(dataPtr), info);
+}
+
+}  // namespace CubbyDNN
