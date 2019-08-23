c8195c4ef407f3395c5b387c195dce9966949614
jwkim98 <jwkim98@kaist.ac.kr>
2019-08-23 11:24:53 +0900

    refactor : rename Sigmoid to Logistic

---
+++ Includes/cubbydnn/Computations/Logistic.hpp
@@ -0,0 +1,134 @@
+//
+// Created by jwkim98 on 8/15/19.
+//
+
+#ifndef CUBBYDNN_LOGISTIC_HPP
+#define CUBBYDNN_LOGISTIC_HPP
+
+//! This file contains different implementations depending on numberSystems
+
+#include <cubbydnn/Tensors/Tensor.hpp>
+#include <cubbydnn/Units/HiddenComputableUnits/HiddenUnit.hpp>
+#include <cubbydnn/Utils/GlobalHyperparams.hpp>
+#include <cubbydnn/Utils/TypeCast.hpp>
+#include <include/universal/posit/posit>
+#include <iostream>
+#include <type_traits>
+
+namespace CubbyDNN
+{
+class LogisticBasic : public HiddenUnit
+{
+    void Compute() override
+    {
+        // TODO : separate types and call correct Calculate implementation
+        assert(m_inputTensorVector.size() == 1 &&
+               m_outputTensorVector.size() == 1);
+        auto numberSystem = m_inputTensorVector.at(0).Info.GetNumberSystem();
+
+        Tensor& sourceTensor = m_inputTensorVector.at(0);
+        Tensor& destTensor = m_outputTensorVector.at(1);
+
+        switch (numberSystem)
+        {
+            case NumberSystem::Float16:
+                Calculate<float>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Float32:
+                Calculate<float>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Float64:
+                Calculate<double>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Int8:
+                Calculate<int8_t>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Int16:
+                Calculate<int16_t>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Int32:
+                Calculate<int32_t>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Int64:
+                Calculate<int64_t>(destTensor, sourceTensor);
+                break;
+            case NumberSystem::Posit8:
+                Calculate<8, UnumBitConfig::Posit8_es>(destTensor,
+                                                       sourceTensor);
+                break;
+            case NumberSystem::Posit16:
+                Calculate<16, UnumBitConfig::Posit16_es>(destTensor,
+                                                         sourceTensor);
+                break;
+            case NumberSystem::Posit32:
+                Calculate<32, UnumBitConfig::Posit32_es>(destTensor,
+                                                         sourceTensor);
+                break;
+            case NumberSystem::Posit64:
+                Calculate<64, UnumBitConfig::Posit64_es>(destTensor,
+                                                         sourceTensor);
+                break;
+        }
+    }
+
+    //TODO : separate implementations by bytes
+    template <typename T, typename std::enable_if_t<
+                              std::is_floating_point<T>::value, T>* = nullptr>
+    void Calculate(Tensor& destTensor, Tensor& sourceTensor)
+    {
+        assert(destTensor.Info.Size() == sourceTensor.Info.Size());
+        assert(destTensor.Info.GetNumberSystem() ==
+               sourceTensor.Info.GetNumberSystem());
+        auto size = destTensor.Info.Size();
+        auto sourcePtr = TypeCast::CastFromTensor<T>(destTensor);
+        auto destPtr = TypeCast::CastFromTensor<T>(sourceTensor);
+        for (size_t count = 0; count < size; ++count)
+        {
+            *(destPtr + count) = 1 / (1 + exp(-*(sourcePtr + count)));
+        }
+    }
+
+    template <typename T, typename std::enable_if_t<std::is_integral<T>::value,
+                                                    T>* = nullptr>
+    void Calculate(Tensor& destTensor, Tensor& sourceTensor)
+    {
+        assert(destTensor.Info.Size() == sourceTensor.Info.Size());
+        assert(destTensor.Info.GetNumberSystem() ==
+               sourceTensor.Info.GetNumberSystem());
+        auto size = destTensor.Info.Size();
+        auto sourcePtr = TypeCast::CastFromTensor<T>(destTensor);
+        auto destPtr = TypeCast::CastFromTensor<T>(sourceTensor);
+        for (size_t count = 0; count < size; ++count)
+        {
+            *(destPtr + count) = 1 / (1 + exp(-*(sourcePtr + count)));
+        }
+    }
+
+    template <size_t nbits, size_t es>
+    void Calculate(Tensor& destTensor, Tensor& sourceTensor)
+    {
+        assert(destTensor.Info.Size() == sourceTensor.Info.Size());
+        assert(destTensor.Info.GetNumberSystem() ==
+               sourceTensor.Info.GetNumberSystem());
+        using positType = typename sw::unum::posit<nbits, es>;
+
+        auto size = sourceTensor.Info.Size();
+        positType sourceValue[size];
+        positType destValue[size];
+        TypeCast::CastFromTensor<nbits, es>(sourceTensor,
+                                                       sourceValue);
+
+        // TODO : parallelize this for loop if possible
+        for (size_t count = 0; count < size; ++count)
+        {
+            destValue[count] =
+                positType(1) /
+                (positType(1) + sw::unum::exp<nbits, es>(-sourceValue[count]));
+        }
+
+        TypeCast::CastToTensor<nbits, es>(destTensor, destValue);
+    }
+};
+}  // namespace CubbyDNN
+
+#endif  // CUBBYDNN_LOGISTIC_HPP
