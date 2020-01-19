65fe37ce8b417afe8a0329ef04ec7f0e5197456b
Chris Ohk <utilForever@gmail.com>
2020-01-19 21:55:58 +0900

    feat(rework): Reset to initial project structure

---
+++ Includes/CubbyDNN/Test.hpp
@@ -0,0 +1,6 @@
+#ifndef CUBBYDNN_TEST_HPP
+#define CUBBYDNN_TEST_HPP
+
+int Add(int a, int b);
+
+#endif 
---
+++ Sources/cubbydnn/Test.cpp
@@ -0,0 +1,6 @@
+#include <CubbyDNN/Test.hpp>
+
+int Add(int a, int b)
+{
+    return a + b;
+} 
