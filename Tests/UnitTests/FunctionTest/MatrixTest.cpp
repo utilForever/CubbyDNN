21ec919deafac536429c0c78022eb7bfe6792916
Justin <jwkim98@kaist.ac.kr>
2020-02-22 00:13:20 +0900

    add : matrix test sessions

---
+++ Tests/UnitTests/FunctionTest/MatrixTest.cpp
@@ -0,0 +1,48 @@
+/// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "MatrixTest.hpp"
+#include "gtest/gtest.h"
+#include <cubbydnn/Computations/Functions/Matrix.hpp>
+#include <iostream>
+
+namespace CubbyDNN
+{
+void TestMatMul()
+{
+    Tensor tensorA =
+        AllocateTensor(TensorInfo({ 1, 1, 3, 3 }, NumberSystem::Float32));
+    Tensor tensorB =
+        AllocateTensor(TensorInfo({ 1, 1, 3, 3 }, NumberSystem::Float32));
+
+    SetData<float>({ 0, 0, 0, 0 }, tensorA, 3.0f);
+    SetData<float>({ 0, 0, 1, 1 }, tensorA, 3.0f);
+    SetData<float>({ 0, 0, 2, 2 }, tensorA, 3.0f);
+
+    SetData<float>({ 0, 0, 0, 0 }, tensorB, 3.0f);
+    SetData<float>({ 0, 0, 1, 1 }, tensorB, 3.0f);
+    SetData<float>({ 0, 0, 2, 2 }, tensorB, 3.0f);
+
+    Tensor output =
+        AllocateTensor(TensorInfo({ 1, 1, 3, 3 }, NumberSystem::Float32));
+
+    Multiply(tensorA, tensorB, output);
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            std::cout << GetData<float>({ 0, 0, i, j }, output) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+TEST(MatrixTest, MatMul)
+{
+    TestMatMul();
+}
+} // namespace CubbyDNN
---
+++ Tests/UnitTests/FunctionTest/MatrixTest.hpp
@@ -0,0 +1,15 @@
+/// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYDNN_MATRIXTEST_HPP
+#define CUBBYDNN_MATRIXTEST_HPP
+
+namespace CubbyDNN
+{
+void TestMatMul();
+}
+
+#endif
