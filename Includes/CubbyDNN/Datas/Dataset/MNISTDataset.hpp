9249e171579bc8604397b7f934cb9bce1da8fbf4
JYPark09 <jyp10987@gmail.com>
2020-03-01 22:39:27 +0900

    feat(data-processing): Create 'MNISTDataset' class

---
+++ Includes/CubbyDNN/Datas/Dataset/MNISTDataset.hpp
@@ -0,0 +1,31 @@
+#ifndef CUBBYDNN_MNIST_DATASET_HPP
+#define CUBBYDNN_MNIST_DATASET_HPP
+
+#include <CubbyDNN/Datas/Image.hpp>
+
+#include <optional>
+#include <string>
+
+namespace CubbyDNN
+{
+class MNISTDataset
+{
+ public:
+    MNISTDataset(const std::string& root, bool train);
+
+    bool IsTrain() const;
+    std::tuple<Image, std::uint32_t> Get(std::size_t index) const;
+    std::optional<std::size_t> Size() const;
+
+ private:
+    void loadImages(const std::string& filename);
+    void loadLabels(const std::string& filename);
+
+    std::uint32_t m_rowSize{ 0 }, m_colSize{ 0 };
+    bool m_isTrain, m_loaded{ false };
+    std::vector<Image> m_images;
+    std::vector<int> m_labels;
+};
+}  // namespace CubbyDNN
+
+#endif  // CUBBYDNN_MNIST_DATASET_HPP
