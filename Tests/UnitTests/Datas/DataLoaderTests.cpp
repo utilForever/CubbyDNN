eb1685a920009a375cb5115afb74af1bcd7af193
JYPark09 <jyp10987@gmail.com>
2020-03-28 02:32:45 +0900

    test: Add unittests for DataLoader

---
+++ Tests/UnitTests/Datas/DataLoaderTests.cpp
@@ -0,0 +1,118 @@
+#include <doctest.h>
+#include <iostream>
+
+#include <CubbyDNN/Datas/DataLoader.hpp>
+#include <CubbyDNN/Datas/Dataset.hpp>
+
+using namespace CubbyDNN;
+
+class TestDataset : public Dataset<TestDataset, std::tuple<FloatTensor, long>>
+{
+ public:
+    OutputType Get(std::size_t index) const override
+    {
+        static std::tuple<FloatTensor, long> arr[] = {
+            std::make_tuple<FloatTensor, long>({ 1, 1, 1, 1 }, 10),
+            std::make_tuple<FloatTensor, long>({ 2, 2, 2, 2 }, 20),
+            std::make_tuple<FloatTensor, long>({ 3, 3, 3, 3 }, 30),
+            std::make_tuple<FloatTensor, long>({ 4, 4, 4, 4 }, 40),
+            std::make_tuple<FloatTensor, long>({ 5, 5, 5, 5 }, 50)
+        };
+
+        return arr[index];
+    }
+
+    std::size_t GetSize() const override
+    {
+        return 5;
+    }
+};
+
+TEST_CASE("[DataLoader] - Big batchsize than dataset size")
+{
+    CHECK_THROWS(
+        DataLoader<TestDataset> loader(std::make_unique<TestDataset>(), 500));
+}
+
+TEST_CASE("[DataLoader] - NotBatched")
+{
+    DataLoader<TestDataset> loader(std::make_unique<TestDataset>());
+
+    CHECK_EQ(loader.GetBatchSize(), 1);
+    CHECK_EQ(loader.GetSize(), 5);
+
+    loader.Begin();
+
+    std::optional<std::tuple<FloatTensor, LongTensor>> batch;
+
+    for (std::size_t i = 0; i < 5; ++i)
+    {
+        batch = loader.Next();
+
+        CHECK_EQ(batch.has_value(), true);
+        CHECK_EQ(std::get<0>(batch.value()).size(), 4 * 1);
+        CHECK_EQ(std::get<0>(batch.value())[0], static_cast<float>(i + 1));
+        CHECK_EQ(std::get<1>(batch.value())[0], (i + 1) * 10);
+    }
+
+    batch = loader.Next();
+    CHECK_EQ(batch.has_value(), false);
+}
+
+TEST_CASE("[DataLoader] - Batched(Drop Last)")
+{
+    DataLoader<TestDataset> loader(std::make_unique<TestDataset>(), 2);
+
+    CHECK_EQ(loader.GetBatchSize(), 2);
+    CHECK_EQ(loader.GetSize(), 2);  // 5 = 2 * 2 + 1(dropped)
+
+    loader.Begin();
+
+    std::optional<std::tuple<FloatTensor, LongTensor>> batch;
+
+    long value = 1;
+    for (std::size_t i = 0; i < 2; ++i)
+    {
+        batch = loader.Next();
+
+        CHECK_EQ(batch.has_value(), true);
+        CHECK_EQ(std::get<0>(batch.value()).size(), 4 * 2);
+
+        for (int j = 0; j < 2; ++j)
+        {
+            CHECK_EQ(std::get<0>(batch.value())[j * 4],
+                     static_cast<float>(value));
+
+            CHECK_EQ(std::get<1>(batch.value())[j], value * 10);
+
+            ++value;
+        }
+    }
+
+    batch = loader.Next();
+    CHECK_EQ(batch.has_value(), false);
+}
+
+TEST_CASE("[DataLoader] - Batched(Not Drop Last)")
+{
+    DataLoader<TestDataset> loader(std::make_unique<TestDataset>(), 5);
+
+    CHECK_EQ(loader.GetBatchSize(), 5);
+    CHECK_EQ(loader.GetSize(), 1);  // 5 = 5 * 1
+
+    loader.Begin();
+
+    auto batch = loader.Next();
+
+    CHECK_EQ(batch.has_value(), true);
+    CHECK_EQ(std::get<0>(batch.value()).size(), 4 * 5);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        CHECK_EQ(std::get<0>(batch.value())[i * 4], static_cast<float>(i + 1));
+        CHECK_EQ(std::get<1>(batch.value())[i], (i + 1) * 10);
+    }
+
+    batch = loader.Next();
+    CHECK_EQ(batch.has_value(), false);
+}
