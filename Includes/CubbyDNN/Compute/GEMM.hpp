474a4ebb1039d8efcaf059005a93f37e2234a108
Chris Ohk <utilforever@gmail.com>
2020-05-24 17:30:02 +0900

    feat(rework): Create class 'GEMM'

---
+++ Includes/CubbyDNN/Compute/GEMM.hpp
@@ -0,0 +1,26 @@
+#ifndef CUBBYDNN_GEMM_HPP
+#define CUBBYDNN_GEMM_HPP
+
+#include <CubbyDNN/Core/Span.hpp>
+
+namespace CubbyDNN::Compute
+{
+class GEMM final
+{
+ public:
+    GEMM() = delete;
+    ~GEMM() noexcept = delete;
+    GEMM(const GEMM& rhs) = delete;
+    GEMM(GEMM&& rhs) noexcept = delete;
+
+    GEMM& operator=(const GEMM& rhs) = delete;
+    GEMM& operator=(GEMM&& rhs) noexcept = delete;
+
+    static void __vectorcall MultiplyAdd(
+        std::size_t maxIndex, std::size_t numRow, std::size_t numColumn,
+        const Core::Span<float> left, const Core::Span<float> right,
+        Core::Span<float> destination) noexcept;
+};
+}  // namespace CubbyDNN::Compute
+
+#endif
---
+++ Sources/CubbyDNN/Compute/GEMM.cpp
@@ -0,0 +1,55 @@
+#include <CubbyDNN/Compute/GEMM.hpp>
+
+#include <immintrin.h>
+
+namespace CubbyDNN::Compute
+{
+void __vectorcall GEMM::MultiplyAdd(std::size_t maxIndex, std::size_t numRow,
+                                    std::size_t numColumn,
+                                    const Core::Span<float> left,
+                                    const Core::Span<float> right,
+                                    Core::Span<float> destination) noexcept
+{
+    const auto* l = left.begin();
+    const auto* r = right.begin();
+    auto* __restrict d = destination.begin();
+
+#pragma omp parallel for schedule(guided) default(shared)                    \
+    num_threads(static_cast <int>(std::max <std::size_t>(                    \
+        1u, std::min <std::size_t>(maxIndex * numRow * numColumn / 1600000u, \
+                                   std::thread::hardware_concurrency()))))
+
+    for (std::int64_t numR = 0; numR < static_cast<std::int64_t>(numRow);
+         ++numR)
+    {
+        for (std::size_t numC = 0; numC < numColumn; ++numC)
+        {
+            std::size_t numIndex = 0;
+            auto sum = _mm256_setzero_ps();
+
+            for (; numIndex + 8 <= maxIndex; numIndex += 8)
+            {
+                sum = _mm256_fmadd_ps(
+                    _mm256_loadu_ps(l + numR * maxIndex + numIndex),
+                    _mm256_loadu_ps(r + numC * maxIndex + numIndex), sum);
+            }
+
+            const auto sum128 = _mm_add_ps(_mm256_extractf128_ps(sum, 1),
+                                           _mm256_castps256_ps128(sum));
+            const auto sum64 =
+                _mm_add_ps(sum128, _mm_movehl_ps(sum128, sum128));
+            const auto sum32 =
+                _mm_add_ss(sum64, _mm_shuffle_ps(sum64, sum64, 0x55));
+            auto numSum = _mm_cvtss_f32(sum32);
+
+            for (; numIndex < maxIndex; ++numIndex)
+            {
+                numSum += l[numR * maxIndex + numIndex] *
+                          r[numC * maxIndex + numIndex];
+            }
+
+            d[numR * numColumn + numC] += numSum;
+        }
+    }
+}
+}  // namespace CubbyDNN::Compute
