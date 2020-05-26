2c3cf0a2654902ae550572825e640d5489aa6b69
Chris Ohk <utilforever@gmail.com>
2020-05-26 23:08:32 +0900

    feat(rework): Add method 'dMultiplyAddRight()'

--- Sources/CubbyDNN/Compute/GEMM.cpp
+++ Sources/CubbyDNN/Compute/GEMM.cpp
@@ -144,4 +144,80 @@ void __vectorcall GEMM::dMultiplyAddLeft(std::size_t maxIndex,
         }
     }
 }
+
+void __vectorcall GEMM::dMultiplyAddRight(
+    std::size_t maxIndex, std::size_t numRow, std::size_t numColumn,
+    const Core::Span<float> gradient, const Core::Span<float> left,
+    Core::Span<float> destination) noexcept
+{
+    const auto* g = gradient.begin();
+    const auto* l = left.begin();
+    auto* __restrict d = destination.begin();
+
+    std::vector<float> gradientTransposed(numRow * numColumn);
+    std::vector<float> leftTransposed(numRow * maxIndex);
+    auto* __restrict gTransposed = gradientTransposed.data();
+    auto* __restrict lTransposed = leftTransposed.data();
+
+#pragma omp parallel default(shared)                                         \
+    num_threads(static_cast <int>(std::max <std::size_t>(                    \
+        1u, std::min <std::size_t>(maxIndex * numRow * numColumn / 1600000u, \
+                                   std::thread::hardware_concurrency()))))
+    {
+#pragma omp for schedule(guided) nowait
+        for (std::int64_t numR = 0; numR < static_cast<std::int64_t>(numRow);
+             ++numR)
+        {
+            for (std::size_t numC = 0; numC < numColumn; ++numC)
+            {
+                gTransposed[numC * numRow + numR] = g[numR * numColumn + numC];
+            }
+        }
+
+#pragma omp for schedule(guided)
+        for (std::int64_t numR = 0; numR < static_cast<std::int64_t>(numRow);
+             ++numR)
+        {
+            for (std::size_t numC = 0; numC < maxIndex; ++numC)
+            {
+                lTransposed[numC * numRow + numR] = l[numR * maxIndex + numC];
+            }
+        }
+
+#pragma omp for schedule(guided)
+        for (std::int64_t numR = 0; numR < static_cast<std::int64_t>(numColumn);
+             ++numR)
+        {
+            for (std::size_t numC = 0; numC < maxIndex; ++numC)
+            {
+                std::size_t numIndex = 0;
+                auto sum = _mm256_setzero_ps();
+
+                for (; numIndex + 8 <= numRow; numIndex += 8)
+                {
+                    sum = _mm256_fmadd_ps(
+                        _mm256_loadu_ps(gTransposed + numR * numRow + numIndex),
+                        _mm256_loadu_ps(lTransposed + numC * numRow + numIndex),
+                        sum);
+                }
+
+                const auto sum128 = _mm_add_ps(_mm256_extractf128_ps(sum, 1),
+                                               _mm256_castps256_ps128(sum));
+                const auto sum64 =
+                    _mm_add_ps(sum128, _mm_movehl_ps(sum128, sum128));
+                const auto sum32 =
+                    _mm_add_ss(sum64, _mm_shuffle_ps(sum64, sum64, 0x55));
+                auto numSum = _mm_cvtss_f32(sum32);
+
+                for (; numIndex < numRow; ++numIndex)
+                {
+                    numSum += gTransposed[numR * numRow + numIndex] *
+                              lTransposed[numC * numRow + numIndex];
+                }
+
+                d[numR * maxIndex + numC] += numSum;
+            }
+        }
+    }
+}
 }  // namespace CubbyDNN::Compute
