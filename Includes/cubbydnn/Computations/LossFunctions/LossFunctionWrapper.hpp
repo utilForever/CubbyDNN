48457a139ade7995d8ae79d5ff40bf4bd91b0971
Justin <jwkim98@kaist.ac.kr>
2020-06-07 18:54:40 +0900

    add : LossFunctionWrapper

---
+++ Includes/cubbydnn/Computations/LossFunctions/LossFunctionWrapper.hpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYDNN_COMPUTE_LOSSFUNCTIONWRAPPER_HPP
+#define CUBBYDNN_COMPUTE_LOSSFUNCTIONWRAPPER_HPP
+#include <cubbydnn/Computations/LossFunctions/LossFunctions.hpp>
+#include <memory>
+#include <unordered_map>
+
+namespace CubbyDNN::Compute
+{
+class LossFunctionWrapper
+{
+ public:
+    const static std::unique_ptr<Loss<float>>& GetFloatLoss(
+        const std::string& name)
+    {
+        return m_floatLossMap.at(name);
+    }
+
+    const static std::unique_ptr<Loss<int>>& GetIntegerLoss(
+        const std::string& name)
+    {
+        return m_integerLossMap.at(name);
+    }
+
+    static void Initialize()
+    {
+        m_floatLossMap["MSE"] = std::make_unique<MSE<float>>();
+        m_integerLossMap["MSE"] = std::make_unique<MSE<int>>();
+    }
+
+ private:
+    static std::unordered_map<std::string,
+                              std::unique_ptr<Loss<float>>>
+        m_floatLossMap;
+
+    static std::unordered_map<std::string, std::unique_ptr<Loss<int>>>
+        m_integerLossMap;
+
+};
+}
+#endif
