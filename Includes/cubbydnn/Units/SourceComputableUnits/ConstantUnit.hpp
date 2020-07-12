f39ca153ee12768f5cb9db6ac779744aab3b12e5
Justin <jwkim98@kaist.ac.kr>
2020-07-12 12:40:43 +0900

    add : ConstantUnit

---
+++ Includes/cubbydnn/Units/SourceComputableUnits/ConstantUnit.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYDNN_GRAPH_CONSTANTUNIT_HPP
+#define CUBBYDNN_GRAPH_CONSTANTUNIT_HPP
+
+#include <cubbydnn/Units/SourceComputableUnits/SourceUnit.hpp>
+
+namespace CubbyDNN::Graph
+{
+class ConstantUnit : public ComputableUnit
+{
+public:
+    ConstantUnit(UnitId unitId, Tensor tensor);
+   ~ConstantUnit() override = default;
+
+    ConstantUnit(const ConstantUnit& constantUnit) = delete;
+    ConstantUnit(ConstantUnit&& constantUnit) noexcept;
+
+    ConstantUnit& operator=(const ConstantUnit& constantUnit) = delete;
+    ConstantUnit& operator=(ConstantUnit&& constantUnit) noexcept = default;
+
+    static ConstantUnit CreateUnit(const UnitMetaData& unitMetaData);
+
+    void Forward() override
+    {
+    }
+
+    void AsyncForward(std::promise<bool> promise) override
+    {
+        promise.set_value(true);
+    }
+
+    void Backward() override
+    {
+    }
+
+    void AsyncBackward(std::promise<bool> promise) override
+    {
+        promise.set_value(true);
+    }
+};
+}
+
+#endif
---
+++ Sources/cubbydnn/Units/SourceComputableUnits/ConstantUnit.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 Chris Ohk, Justin Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include<cubbydnn/Units/SourceComputableUnits/ConstantUnit.hpp>
+
+namespace CubbyDNN::Graph
+{
+ConstantUnit::ConstantUnit(UnitId unitId, Tensor tensor)
+    : ComputableUnit(std::move(unitId), tensor.NumericType, {},
+                     {}, std::move(tensor), {})
+{
+}
+
+ConstantUnit::ConstantUnit(ConstantUnit&& constantUnit) noexcept
+    : ComputableUnit(std::move(constantUnit))
+{
+}
+
+ConstantUnit ConstantUnit::CreateUnit(const UnitMetaData& unitMetaData)
+{
+    return ConstantUnit(unitMetaData.Id(),
+                        std::move(unitMetaData.GetInternalTensor("constant")));
+}
+}
