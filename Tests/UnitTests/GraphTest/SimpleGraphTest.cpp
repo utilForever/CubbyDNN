6b8f29420948f8e24832337666341b54728f07a1
Justin <jwkim98@kaist.ac.kr>
2020-08-14 21:02:09 +0900

    add : simple graph test

---
+++ Tests/UnitTests/GraphTest/SimpleGraphTest.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2020, Jaewoo Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Takion/FrontEnd/Model.hpp>
+#include "SimpleGraphTest.hpp"
+
+namespace Takion::Test
+{
+using namespace FrontEnd;
+
+void SimpleGraphTest()
+{
+    Model<float> model(Compute::Device(0, Compute::DeviceType::CPU, "device0"),
+                       10);
+
+    auto tensor =
+        model.Constant(Shape({ 10 }), std::vector<float>(90, 3), "input");
+    const auto label = model.Constant(Shape({ 3 }), std::vector<float>(30, 1),
+                                      "label");
+
+    tensor = model.Dense(tensor, 3);
+    tensor = model.ReLU(tensor);
+    model.MSE(tensor, label, "MseLoss");
+
+    model.Compile("SGD", Parameter({}, { { "epsilon", 0.01f } }, {}));
+    model.Fit(100);
+}
+} // namespace Takion::Test
---
+++ Tests/UnitTests/GraphTest/SimpleGraphTest.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020, Jaewoo Kim
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef TAKION_TEST_SIMPLEGRAPHTEST_HPP
+#define TAKION_TEST_SIMPLEGRAPHTEST_HPP
+
+namespace Takion::Test
+{
+void SimpleGraphTest();
+}
+
+#endif
